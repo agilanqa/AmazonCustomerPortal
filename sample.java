ccb6224e6d7ae902b9cf76bcbaf301cdf1a728d9
agilanqa <agilankrishnam@gmail.com>
2022-10-12 15:16:49 +0530

    My First Java Program

---
+++ sample.java
@@ -0,0 +1,20 @@
+Class Test{
+int x, y;
+
+public void test(){
+
+System.out.println("Hi....");
+}
+
+public static void main(String args[]){
+
+Test test = new Test();
+
+test.x= 10;
+test.y= 20;
+
+test.test();
+
+}
+
+}
